7efdf06e610418c513b995f841857c67cc85b184
SenorToru <dev@toru-leathers.com>
2026-09-15 22:57:14 +0900

    feat: set neovim auto indent as 2 spaces.

--- flake.nix
+++ flake.nix
@@ -5,36 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
-    # 原生 Zen Browser Flake 源
+# 原生 Zen Browser Flake 源
     zen-browser.url = "github:youwen5/zen-browser-flake";
-    # optional, but recommended if you closely follow NixOS unstable so it shares
-    # system libraries, and improves startup time
-    # NOTE: if you experience a build failure with Zen, the first thing to check is to remove this line!
+# optional, but recommended if you closely follow NixOS unstable so it shares
+# system libraries, and improves startup time
+# NOTE: if you experience a build failure with Zen, the first thing to check is to remove this line!
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nix-flatpak,
-      zen-browser,
-      ...
-    }@inputs:
-    {
-      nixosConfigurations = {
-        # ThinkPad X1 Yoga 入口
-        thinkpad = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          # 将 inputs 透传进所有模块，以便在 desktop-gnome.nix 中直接取用
-          specialArgs = { inherit inputs; };
-          modules = [
-            nix-flatpak.nixosModules.nix-flatpak
+  {
+    self,
+    nixpkgs,
+    nix-flatpak,
+    zen-browser,
+    ...
+  }@inputs:
+  {
+    nixosConfigurations = {
+# ThinkPad X1 Yoga 入口
+      thinkpad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+# 将 inputs 透传进所有模块，以便在 desktop-gnome.nix 中直接取用
+        specialArgs = { inherit inputs; };
+        modules = [
+          nix-flatpak.nixosModules.nix-flatpak
             ./hosts/thinkpad/default.nix
-          ];
-        };
-
-        # 将来华硕笔记本只需在此增加一个 asus 条目，引入 ./hosts/asus/default.nix
+        ];
       };
+
+# 将来华硕笔记本只需在此增加一个 asus 条目，引入 ./hosts/asus/default.nix
     };
+  };
 }
--- modules/development.nix
+++ modules/development.nix
@@ -3,7 +3,28 @@
 {
   environment.systemPackages = with pkgs; [
     vscode
-    nil
-    nixfmt
+      nil
+      nixfmt
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    configure = {
+      # Set NeoVim indent rules.
+      customRC = ''
+        lua << EOF
+        vim.opt.tabstop = 2
+        vim.opt.softtabstop = 2
+        vim.opt.shiftwidth = 2
+        vim.opt.expandtab = true
+        vim.opt.autoindent = true
+        vim.opt.smartindent = true
+        EOF
+        '';
+    };
+  };
 }
